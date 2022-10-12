7a81fd2055010458f0f652fed339876736fd4156
St-Gong <112709874+St-Gong@users.noreply.github.com>
2022-10-12 16:03:25 -0400

    Add files via upload

---
+++ Ergodic_Climate/Code/2.Pre_mon_H.R
@@ -0,0 +1,134 @@
+# 1.Clean up workspace and load or install necessary packages---------
+  rm(list=ls())
+  want <- c("rasterVis","rgdal","sf","raster","RColorBrewer","sp","maps","maptools","rgeos",
+            "utils","ncdf4","tmaptools","rworldmap","Matrix","ggplot2","ggpubr")
+  need <- want[!(want %in% installed.packages()[,"Package"])]
+  if (length(need)) install.packages(need)
+  lapply(want, function(i) require(i, character.only=TRUE))
+  rm(want, need)
+
+# 2.Working directories--------
+  dir <- list()
+  dir$root <- dirname(getwd())
+  dir$Pre_monthly <- paste(dir$root,"/Data/Raw_Data/Presciption_monthly",sep="")
+  dir$map <- paste(dir$root,"/Data/Raw_Data/Chinese_Map",sep="")
+  dir$output <- paste(dir$root,"/Output",sep="")
+  dir$elevation <- paste(dir$root,"/Data/Raw_Data/Elevation",sep="")
+  
+# 3.Download data from "http://hydrology.princeton.edu/data/pgf/v3/0.25deg/daily/" -----
+#Monthly data
+  url <- "http://hydrology.princeton.edu/data/pgf/v2/0.5deg/monthly/prcp_monthly_1901-2012.nc"
+  destfile <- paste(dir$Pre_monthly,"/prcp_monthly_1901-2012.nc",sep="")
+  download.file(url, destfile=destfile, method="curl", quiet = FALSE, mode = "w")
+
+# 4.Read Data and Manipulation--------
+  flist <- list.files(dir$Pre_monthly, full.names=T)
+  Pre_mon <- brick(flist[1])
+  cn <- readOGR(paste(dir$map,"/Province.shp",sep=""))
+  
+  elevation <- raster(paste(dir$elevation,"/elevation_0.5deg.nc",sep=""))
+  land <- elevation
+  land[] <- ifelse(is.na(elevation[]), 0, 1)  
+  id <- land
+  id[] <- 1:length(id)
+  id[land[]==1] <- NA
+  location <- id[!is.na(id)]
+  Pre_mon[][location,] <- NA
+
+  e <-c(65,137,10,60)
+  Pre_mon_cn <- crop(Pre_mon,e)
+  cn <- crop(cn,e)
+
+  name <- c()
+  k <- 1
+  for (i in 1901:2012){
+    for (j in 1:12){
+      name[k] <- paste(i, "_", j, sep="") 
+      k <- k+1
+    }
+  }
+  names(Pre_mon_cn) <- name
+
+# 6.Function we need----
+# This part includes two function:
+# 1) A simple function used to shift a column of dataframe upward (To create Xt and Xt-1 when using regression)
+# 2) Function to calculate H
+## 6.1.Function to shift a column upward-------
+  shift <- function(x, n){
+    c(x[-(seq(n))], rep(NA, n))
+  }
+
+## 6.2.Function to calculate H--------
+  Calculate_H <- function(tmp){
+    data <- cbind(tmp,tmp)
+    colnames(data) = c("tmp1", "tmp2") 
+    data <- as.data.frame(data)
+    data$tmp2 <- shift(data$tmp1, 1) 
+    reg<-lm(data$tmp2~data$tmp1,data=data)  
+    coe <- summary(reg)$coefficients
+    beta <- coe[2,1]
+    alpha <- coe[1,1]
+    a <- 1-beta
+    sigma <- 0
+    for (i in 1:112){
+      t <- (1-a)^(2*i) 
+      sigma <- sigma + t
+    }
+    H <- 1/2*(log(1+sigma)/log(112))
+    return(H)
+  }
+
+
+# 7.Main Code-------
+  H_list <- c()
+  tmp_list <- c()
+  month <- c()
+  for (j in 1:12){
+    for (i in 1:112){
+      month[i] <- paste("X", i+1900, "_", j, sep="") 
+    }
+    Pre_month<- Pre_mon_cn[][,month]
+    for (i in 1:dim(Pre_month[])[1]){
+      tmp <- Pre_month[][i,]
+      if (sum(!is.na(tmp))>=2){
+        if (sum(tmp==0)==length(tmp)) {
+          tmp_list[i] <- 0
+        }
+        else{
+          tmp_list[i] <- Calculate_H(tmp)
+        }
+      }
+      else {
+        tmp_list[i] <- NA
+      }
+    }
+    H_list <- cbind(H_list,tmp_list)
+  }
+
+  Pre_mon_cn[][,seq(1,12)] <- H_list
+
+# 8.Draw The map ----- 
+  png(paste(dir$output,"/Pre_H.png",sep=""), width=1000*4, height = 1000*3, res=80*4)
+  
+  for (i in 1:12){
+    num <- paste("p",i,sep="")
+    plot_data <- as.data.frame(Pre_mon_cn[[i]],xy=TRUE)
+    colnames(plot_data) <- c("x","y","value")
+    p<-  ggplot()+
+      geom_raster(data=plot_data,aes(x=x,y=y,fill=value))+
+      scale_fill_gradientn(colours=c(brewer.pal(9,'OrRd')[1],brewer.pal(9,'OrRd')[6]), na.value="white") +
+      theme_classic()+
+      theme(axis.line = element_blank(),
+            legend.key.width=unit(3,"cm"),
+            legend.title=element_blank())+
+      xlab(NULL)+
+      ylab(NULL)+
+      geom_polygon(data=cn,aes(long, lat, group = group, fill = hole),colour = "black", fill = NA, size=0.1)
+  
+    assign(num,p)
+  }
+
+  ggarrange(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12, nrow = 3, ncol=4, common.legend = TRUE,labels = month.abb[1:12],legend = "bottom",vjust=0.3, font.label = list(size = 10))+
+    theme(plot.margin = margin(1,0.5,1,0.5, "cm")) 
+
+  dev.off()
